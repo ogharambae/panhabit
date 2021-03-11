41ccc4329707a258dd85aea086f067b5a6aa19d7
HANNAH KUO <hannahkuo0205@gmail.com>
2021-03-10 16:17:22 -0800

    added firebase api

---
+++ scripts/firebase_api_grp7.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyB7hv0t0YOf6-dsTEUyGFq2VejEnZncCL8",
+    authDomain: "panhabit.firebaseapp.com",
+    projectId: "panhabit",
+    storageBucket: "panhabit.appspot.com",
+    messagingSenderId: "217940611964",
+    appId: "1:217940611964:web:390ac6a01c603632868af9"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
