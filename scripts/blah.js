4ccd63b6c1ff569680aceb7fec638888d6e93f32
HyunBae Choi <hyunbaechoi93@gmail.com>
2021-03-23 15:05:22 -0700

    Implemented function to show user name and pet when they log it

--- scripts/blah.js
+++ scripts/blah.js
@@ -1,1 +1,16 @@
-
+function sayHello() {
+    firebase.auth().onAuthStateChanged(function (somebody) {
+        if (somebody) {
+            //console.log(somebody.uid);
+            db.collection("users")
+                .doc(somebody.uid)
+                .get()
+                .then(function (doc) {
+                    //console.log(doc.data().name);
+                    var n = doc.data().name;
+                    $("#name-goes-here").text(n);
+                })
+        }
+    })
+}
+sayHello();
