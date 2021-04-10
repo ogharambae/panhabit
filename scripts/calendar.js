c4afdef176755e1274fa0d7708c00bb352722441
HANNAH KUO <hannahkuo0205@gmail.com>
2021-04-09 17:54:52 -0700

    Add calendar html & js

---
+++ scripts/calendar.js
@@ -0,0 +1,24 @@
+function showAllMyHabitsOnThisDay1(day) {
+    firebase.auth().onAuthStateChanged(function (user) {
+        db.collection("users").doc(user.uid)
+            .get()
+            .then(function (doc) {
+                console.log(doc.data());
+
+                var habitlist = doc.data()[day];   //index into document with "monday" as "key"
+                console.log("Habit list for " + day + " is " + habitlist);
+
+                console.log(day + " habits are " + habitlist); //index is the key "monday"
+                $("#calendar-habits-go-here").append("<p> " + day + ": " + habitlist + "</p>")
+
+                if (habitlist) {   //only display if the list is not empty
+                    habitlist.forEach(function (h) {
+                        $("#" + day + "-habits-go-here").append("<p>" + h + "</p>");
+                    })
+                }
+            })
+    })
+}
+showAllMyHabitsOnThisDay1("monday");
+showAllMyHabitsOnThisDay1("tuesday");
+showAllMyHabitsOnThisDay1("wednesday");
